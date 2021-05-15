4859160e34ab33592cb37b7424c90262860c808c
Glyn <glyn@gitlab.home.local>
2021-05-15 21:44:49 +0100

    Update json-rpc to handle sort tags.

--- xbmc/interfaces/json-rpc/AudioLibrary.cpp
+++ xbmc/interfaces/json-rpc/AudioLibrary.cpp
@@ -63,7 +63,7 @@ JSONRPC_STATUS CAudioLibrary::GetProperties(const std::string &method, ITranspor
       property = (int)BLANKARTIST_ID;
     else if (propertyName == "librarylastupdated")
       property = musicdatabase.GetLibraryLastUpdated();
-    else if (propertyName == "librarylastcleaned")  
+    else if (propertyName == "librarylastcleaned")
       property = musicdatabase.GetLibraryLastCleaned();
     else if (propertyName == "artistlinksupdated")
       property = musicdatabase.GetArtistLinksUpdated();
@@ -251,7 +251,7 @@ JSONRPC_STATUS CAudioLibrary::GetAlbums(const std::string &method, ITransportLay
   std::set<std::string> fields;
   if (parameterObject.isMember("properties") && parameterObject["properties"].isArray())
   {
-    for (CVariant::const_iterator_array field = parameterObject["properties"].begin_array(); 
+    for (CVariant::const_iterator_array field = parameterObject["properties"].begin_array();
       field != parameterObject["properties"].end_array(); field++)
       fields.insert(field->asString());
   }
@@ -477,7 +477,7 @@ JSONRPC_STATUS CAudioLibrary::GetSongs(const std::string &method, ITransportLaye
 
   int start, end;
   HandleLimits(parameterObject, result, total, start, end);
-  
+
   return OK;
 }
 
@@ -615,7 +615,7 @@ JSONRPC_STATUS CAudioLibrary::GetGenres(const std::string &method, ITransportLay
   std::set<std::string> additionalProperties;
   if (CheckForAdditionalProperties(parameterObject["properties"], checkProperties, additionalProperties))
     sourcesneeded = (additionalProperties.find("sourceid") != additionalProperties.end());
-   
+
   CFileItemList items;
   if (!musicdatabase.GetGenresJSON(items, sourcesneeded))
     return InternalError;
@@ -827,6 +827,8 @@ JSONRPC_STATUS CAudioLibrary::SetAlbumDetails(const std::string &method, ITransp
 
   if (ParameterNotNull(parameterObject, "title"))
     album.strAlbum = parameterObject["title"].asString();
+  if (ParameterNotNull(parameterObject, "sorttitle"))
+    album.strAlbumSort = parameterObject["sorttitle"].asString();
   if (ParameterNotNull(parameterObject, "displayartist"))
     album.strArtistDesc = parameterObject["displayartist"].asString();
   // Set album sort string before processing artist credits
@@ -932,12 +934,18 @@ JSONRPC_STATUS CAudioLibrary::SetSongDetails(const std::string &method, ITranspo
 
   if (ParameterNotNull(parameterObject, "title"))
     song.strTitle = parameterObject["title"].asString();
+  if (ParameterNotNull(parameterObject, "sorttitle"))
+    song.strTitleSort = parameterObject["sorttitle"].asString();
 
   if (ParameterNotNull(parameterObject, "displayartist"))
     song.strArtistDesc = parameterObject["displayartist"].asString();
   // Set album sort string before processing artist credits
   if (ParameterNotNull(parameterObject, "sortartist"))
     song.strArtistSort = parameterObject["sortartist"].asString();
+  if (ParameterNotNull(parameterObject, "sortalbumartist"))
+    song.SetAlbumArtistSort(parameterObject["sortalbumartist"].asString());
+  if (ParameterNotNull(parameterObject, "sortalbum"))
+    song.strAlbumSort = parameterObject["sortalbum"].asString();
 
   // Match up artist names and mbids to make new artist credits
   // Mbid values only apply if there are names
@@ -1312,7 +1320,7 @@ JSONRPC_STATUS CAudioLibrary::GetAdditionalSongDetails(const CVariant &parameter
       }
     }
     if (additionalProperties.find("sourceid") != additionalProperties.end())
-    {      
+    {
       musicdatabase.GetSourcesBySong(item->GetMusicInfoTag()->GetDatabaseId(), item->GetPath(), item.get());
     }
     if (item->GetMusicInfoTag()->GetAlbumId() > 0)
